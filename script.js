d04d82526c5f25586477cc5af35f9a13d42504a6
Alex Grafen <alex.grafen@gmail.com>
2022-02-21 14:23:46 +0000

    make function responsive to user input

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let paragraph = document.querySelector("p");
-
-
+let form = document.querySelector("form");
+let inputTitle = document.getElementById("title");
+console.log(inputTitle);
 
 function gutenbergFetcher() {
-    fetch(`https://gutendex.com/books?ids=11,12,13`)
-  .then((response) => {
-    if(!response.ok) {throw new Error ('problem calling API');}
-    return response.json();
-  })
-    .then((response) => {
-    console.log(response.results[0]);
-    let result = response.results[0];
-    return result;
-    })
-    .then((result) => {
-    let translatorNum = result.translators.length;
-    console.log(translatorNum);
-    return translatorNum;
-    })
-  .catch((error) => {
-    console.log(error.message)})
+let inputTitleValue = inputTitle.value;
+console.log(inputTitleValue);
+fetch(`https://gutendex.com/books?search=${inputTitleValue}`)
+.then((response) => {
+if(!response.ok) {throw new Error ('problem calling API');}
+return response.json();
+})
+.then((response) => {
+console.log(response.results[0]);
+let result = response.results[0];
+return result;
+})
+.then((result) => {
+let translatorNum = result.translators.length;
+console.log(translatorNum);
+return translatorNum;
+})
+.catch((error) => {
+console.log(error.message)})
 }
 
-window.addEventListener("click", gutenbergFetcher)
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    gutenbergFetcher();
+})
